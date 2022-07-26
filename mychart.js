717a22f5815d70d3ff9299a8d559a0fe85b6eed8
gregmontalvo <gregorio.montalvo@gmail.com>
2022-07-26 22:52:11 +0200

    changes in javascript tests

--- mychart.js
+++ mychart.js
@@ -1,4 +1,12 @@
-const ctx = document.getElementById('myChart');
-const ctx = document.getElementById('myChart').getContext('2d');
-const ctx = $('#myChart');
-const ctx = 'myChart';
+// const ctx = document.getElementById('myChart');
+// const ctx = document.getElementById('myChart').getContext('2d');
+// const ctx = $('#myChart');
+// const ctx = 'myChart';
+
+let title = document.getElementsByClassName('assetid');
+
+function changeTitle (){
+    title.style.color = 'red'
+};
+
+title.onclick = changeTitle
