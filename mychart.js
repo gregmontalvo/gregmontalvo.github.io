833caf9c58de9f28e1e604116b798f7cff5ad744
gregmontalvo <gregorio.montalvo@gmail.com>
2022-09-15 16:42:35 +0200

    aftersummer

--- mychart.js
+++ mychart.js
@@ -1,9 +1,10 @@
-// const ctx = document.getElementById('myChart');
-// const ctx = document.getElementById('myChart').getContext('2d');
-// const ctx = $('#myChart');
-// const ctx = 'myChart';
+const ctx = document.getElementById('myChart');
+const ctx = document.getElementById('myChart').getContext('2d');
+const ctx = $('#myChart');
+const ctx = 'myChart';
 
-let title = document.getElementsByClassName('assetid');
+
+let title = document.getElementsByClassName('email')[0];
 
 function changeTitle (){
     title.style.color = 'red'
